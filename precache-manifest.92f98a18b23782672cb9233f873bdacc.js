b298ee0511af1c495564df42ee19380d4c38522d
Ritik-cpu <ritikkumar753391@gmail.com>
2021-12-22 20:52:19 +0530

    Updates

---
+++ precache-manifest.92f98a18b23782672cb9233f873bdacc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a771de0c350aeca3bed713ff7592881",
+    "url": "/chatme/index.html"
+  },
+  {
+    "revision": "1e93dc7ad18e4c20fdd8",
+    "url": "/chatme/static/css/main.e82e295d.chunk.css"
+  },
+  {
+    "revision": "861b5246b4a3746d6c70",
+    "url": "/chatme/static/js/2.2716bcfc.chunk.js"
+  },
+  {
+    "revision": "0cf28665e6e113cb7910ea3c2058bef6",
+    "url": "/chatme/static/js/2.2716bcfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e93dc7ad18e4c20fdd8",
+    "url": "/chatme/static/js/main.d9cea81c.chunk.js"
+  },
+  {
+    "revision": "bd86500053617ae883df",
+    "url": "/chatme/static/js/runtime-main.ca353142.js"
+  }
+]);
